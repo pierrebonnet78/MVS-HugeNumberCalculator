e025f949accc1cf678f12cdf59aadfaf1f45f970
pierrebonnet78 <60656840+pierrebonnet78@users.noreply.github.com>
2020-11-02 18:11:49 +0100

    test to verify that the display is working.

--- main.c
+++ main.c
@@ -1,83 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#define MAXCHAR 10000
-
-long decimalToBinary(int decimalnum)
-{
-    long binarynum = 0;
-    int rem, temp = 1;
-
-    while (decimalnum!=0)
-    {
-        rem = decimalnum%2;
-        decimalnum = decimalnum / 2;
-        binarynum = binarynum + rem*temp;
-        temp = temp * 10;
-    }
-    return binarynum;
-}
-
-void letterToByte(char input[], long bytes[]){
-
-    int len = strlen(input);
-    int ascii[len];
-    int i = 0;
-    for(i; i <len; i++){
-        ascii[i] = (int)input[i];
-    }
-    for(i = 0; i<len; i++){
-        long temp = decimalToBinary(ascii[i]);
-        bytes[i] = temp;
-    }
-    
-}
-
-void readtext(char* output){
-    FILE *fp;
-    char str[MAXCHAR];
-    char* filename = "C:\\Users\\pierr\\OneDrive - Efrei\\Documents\\EFREI\\S03\\Algo\\C\\Project 1 Huffman Coding\\Alice.txt";
- 
-    fp = fopen(filename, "r");
-    if (fp == NULL){
-        printf("Could not open file %s",filename);
-    }
-    else{
-        while (fgets(str, MAXCHAR, fp) != NULL){
-            strncat(output, str, (sizeof(output) - strlen(str)));
-        }
-        fclose(fp);
-    }
-}
-
-void writetext(long bytes[], int size){
-    int num;
-   FILE *fp;
-   fp = fopen("C:\\Users\\pierr\\OneDrive - Efrei\\Documents\\EFREI\\S03\\Algo\\C\\Project 1 Huffman Coding\\Output.txt","w");
-   if(fp == NULL)
-   {
-      printf("\nError, Cannot open the file !");                
-   }
-   else{
-       for(int i = 0 ; i<size ; i++){
-           fprintf(fp,"%ld ",bytes[i]);
-           if((i+2)%10 == 1){
-               fprintf(fp,"\n");
-           }
-       }
-    fclose(fp);
-   }
-}
 
 
-int main(){
-    
-    char *text[MAXCHAR];
-    readtext(text);    // read the .txt file and store it the array 'text'.
-    int len = strlen(text);
-    long bytes[len];
-    letterToByte(text, bytes);  // convert each letter of the array 'text' in its binary representation and store them into array 'bytes'.
-    writetext(bytes, len);     // write on an empty file 'Output.txt' what the array 'bytes' contains.  
-    return 0;
+int main(void){
+	HugeUnsignedInt* hugeNumber = createHugeUnsignedIntFromString("123456789012345678901234567890");
+	printHugeUnsignedInt (hugeNumber);
+	hugeNumber = createHugeUnsignedIntFromString("0000000123456789012345678901234567890");
+	printHugeUnsignedInt (hugeNumber);
+	return 0;
 }
 
